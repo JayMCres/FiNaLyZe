617d4037007f1e5ec578b2f92e52f200ae57e10c
JayMCres <crescenzojason@yahoo.com>
2019-11-25 22:25:44 -0500

    Created and got news list functionality working..need to cleanup for viewing

--- frontend/src/Components/HomepageComps/NewsComponents/NewsItem.js
+++ frontend/src/Components/HomepageComps/NewsComponents/NewsItem.js
@@ -2,12 +2,11 @@ import React, { Component } from "react";
 
 import {
   Image,
-  Button,
-  Table,
   Segment,
   Grid,
   List,
-  Message
+  Message,
+  Container
 } from "semantic-ui-react";
 
 import Linkify from "react-linkify";
@@ -15,7 +14,47 @@ import Linkify from "react-linkify";
 class NewsItem extends Component {
   render() {
     console.log("news Props", this.props);
-    return <div>Test</div>;
+    const source = this.props.source.name.toUpperCase();
+    return (
+      <Segment inverted>
+        <Message color="blue">
+          <List divided animated verticalAlign="middle">
+            <List.Item>
+              {" "}
+              <Grid columns={2} compact divided>
+                <Grid.Row>
+                  <Grid.Column width={2}>
+                    <Image
+                      centered
+                      bordered
+                      // floated="left"
+                      size="small"
+                      src={this.props.urlToImage}
+                      // style={{ margin: "2em 2em 2em -4em" }}
+                    />
+                  </Grid.Column>
+                  <Grid.Column width={14}>
+                    <Message floated="left" size="small" info>
+                      <Message.Header>
+                        {this.props.title}-{source}
+                      </Message.Header>
+                      <p>{this.props.description}</p>
+                      <Message.Header>
+                        <Linkify>
+                          <p>Source:{this.props.url}</p>
+                        </Linkify>
+                      </Message.Header>
+                      <br />
+                      <div>{this.props.publishedAt}</div>
+                    </Message>
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid>
+            </List.Item>
+          </List>
+        </Message>
+      </Segment>
+    );
   }
 }
 
